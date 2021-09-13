5f3ff16d2ddf028fc49680b03ed47b26cc17cbf3
tsukanov_roman <roman.tsukanov@devexpress.com>
2021-09-13 18:44:54 +0300

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- default badges list -->
+![](https://img.shields.io/endpoint?url=https://codecentral.devexpress.com/api/v1/VersionRange/340354634/20.2.6%2B)
+[![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
+<!-- default badges end -->
+# Getting Started with DevExtreme PivotGrid
+
+This repository stores the source code for the [Getting Started with PivotGrid](https://js.devexpress.com/Documentation/Guide/UI_Components/PivotGrid/Getting_Started_with_PivotGrid/) tutorial.
---
+++ jQuery/src/index.js
@@ -0,0 +1,75 @@
+$(function () {
+    $("#pivotGridContainer").dxPivotGrid({
+        // Bind to Data
+        dataSource: {
+            store: {
+                type: "xmla",
+                url: "https://demos.devexpress.com/Services/OLAP/msmdpump.dll",
+                catalog: "Adventure Works DW Standard Edition",
+                cube: "Adventure Works"
+            },
+            fields: [{
+                dataField: "[Product].[Category]",
+                area: "row"
+            },
+            // Group Data
+            {
+                dataField: "[Product].[Subcategory]",
+                area: "row"
+            }, {
+                dataField: "[Ship Date].[Calendar Year]",
+                area: "column",
+                // Filter Data
+                filterValues: [["CY 2003"], ["CY 2004"]]
+            },
+            // Group Data
+            {
+                dataField: "[Ship Date].[Month of Year]",
+                area: "column"
+            },
+            // Configure Summaries
+            {
+                dataField: "[Measures].[Sales Amount]",
+                area: "data",
+                format: "currency"
+            }, {
+                dataField: "[Measures].[Tax Amount]",
+                area: "data",
+                format: "currency"
+            }]
+        },
+        // Sort Data - By Field Values
+        allowSorting: true,
+        // Sort Data - By Summary Values
+        allowSortingBySummary: true,
+        // Filter Data
+        allowFiltering: true,
+        // Display the Field Panel
+        fieldPanel: {
+            visible: true,
+            showFilterFields: false
+        },
+        // Configure the Field Chooser
+        fieldChooser: {
+            // enabled: true
+        },
+        // Export to Excel
+        export: {
+            enabled: true
+        },
+        onExporting: function(e) {
+            const workbook = new ExcelJS.Workbook();
+            const worksheet = workbook.addWorksheet('Sales');
+            
+            DevExpress.excelExporter.exportPivotGrid({
+                component: e.component,
+                worksheet: worksheet
+            }).then(function() {
+                workbook.xlsx.writeBuffer().then(function(buffer) {
+                    saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Sales.xlsx');
+                });
+            });
+            e.cancel = true;
+        }
+    });  
+});
