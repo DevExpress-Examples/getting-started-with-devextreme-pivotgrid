b5d186be2c05c2b8b4d0dfdf0f5c174445c23acc
tsukanov_roman <roman.tsukanov@devexpress.com>
2021-09-14 19:05:22 +0300

    Add React

--- React/src/App.js
+++ React/src/App.js
@@ -1,21 +1,53 @@
-import { useState } from 'react';
-
 import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
+import 'devextreme/dist/css/dx.light.css';
 import './App.css';
 
-import Button from 'devextreme-react/button';
+import {
+    PivotGrid,
+    FieldPanel,
+    // FieldChooser,
+    Export
+} from 'devextreme-react/pivot-grid';
 
+import AdventureWorksService from './adventureworks.service';
+import { exportPivotGrid } from 'devextreme/excel_exporter';
+import { Workbook } from 'exceljs';
+import saveAs from 'file-saver';
+
+const dataSource = AdventureWorksService.getPivotGridDataSource();
+const onExporting = (e) => {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Sales');
+
+    exportPivotGrid({
+        component: e.component,
+        worksheet: worksheet
+    }).then(function() {
+        workbook.xlsx.writeBuffer().then(function(buffer) {
+            saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Sales.xlsx');
+        });
+    });
+    e.cancel = true;
+}
 
 function App() {
-  var [count, setCount] = useState(0);
-  const clickHandler = () => {
-    setCount(count + 1);
-  }
   return (
-    <div className="App">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
-    </div>
+      <PivotGrid
+        id="pivot-grid"
+        dataSource={dataSource}
+        allowSorting={true}
+        allowSortingBySummary={true}
+        allowFiltering={true}
+        onExporting={onExporting}>
+        <FieldPanel
+            visible={true}
+            showFilterFields={false}
+        />
+        {/* <FieldChooser
+            enabled={true}
+        /> */}
+        <Export enabled={true} />
+      </PivotGrid>
   );
 }
 
